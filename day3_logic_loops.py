2d238d664afa1065a8e59599f0b4c627eb6fde8e
Josh Sztanko <jsztanko@gmail.com>
2025-11-19 11:46:37 -0800

    Day 3: Conditional logic and loops

---
+++ day3_logic_loops.py
@@ -0,0 +1,10 @@
+# Even or odd checker
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Loop example
+for i in range (60,-10, -3):
+    print("Counting:", i)
