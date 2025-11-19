fce584eb7800b38ce51e4204467cbcb88104894d
Josh Sztanko <jsztanko@gmail.com>
2025-11-19 11:34:15 -0800

    Day 2: Added Python basics calculator script

---
+++ day2_basics.py
@@ -0,0 +1,8 @@
+# Simple calculator
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+num4 = float(input("Enter fourth number: "))
+num5 = float(input("Enter fifth number: "))
+sum = num1 + num2 - num3 * num4 + num5
+print("Sum", sum)
